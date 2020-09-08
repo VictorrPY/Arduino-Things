c11e8aaa98e24456f821d08f8715b0a464923892
Victor <67214928+VictorrPY@users.noreply.github.com>
2020-09-08 00:18:43 -0300

    This just turn on a LED

--- LED.ino
+++ LED.ino
@@ -1,1 +1,12 @@
+int pin = 4 //PUT HERE THE PIN WHERE IS THE LED CONNECTED
 
+void setup() {
+  pinMode(pin, OUTPUT); //CONNECT THE LED
+}
+
+void loop() {
+  digitalWrite(pin, HIGH); //TURN ON THE LED
+  delay(1000); //WAIT 1 SECOND, 1000 IN MILLISECONDS
+  digitalWrite(pin, LOW); //TURN OFF THE LED
+  delay(1000); //WAIT ANOTHER SECOND
+}
